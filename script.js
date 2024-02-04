5950b3993d02f623d4b6e91be5b3f5a672699f00
Mayank Deep <mayankdeep217@gmail.com>
2024-02-04 21:13:34 +0530

    adding the files

---
+++ script.js
@@ -0,0 +1,6 @@
+        // these code will not run here as document is not defines in JS it's provided by Browser
+
+
+// let x = document.getElementById("outer").innerHTML;
+// let x = document.getElementById("outer").innerText;
+// let x = document.getElementById("outer").textContent;
